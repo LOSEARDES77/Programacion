c21eddec98480f0ab139d2d64977c3b6ffd616ef
LOSEARDES77 <loseardes77@gmail.com>
2023-09-22 10:44:34 +0200

    Ejercicio 4 hecho

--- U1_P01_4/src/main/java/org/example/Main.java
+++ U1_P01_4/src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.util.Calendar;
 import java.util.Scanner;
+import java.util.Date;
+
+/*
+* 4.	Escriba un programa que pregunte la edad al usuario y la fecha actual, y el día y mes de su cumpleaños, y en base a esa información calcule su año de nacimiento.
+* */
 
 public class Main {
     public static void main(String[] args) {
+        final Calendar CALENDARIO = Calendar.getInstance();
+        final short AÑO = (short) CALENDARIO.get(Calendar.YEAR);
+        final short MES = (short) CALENDARIO.get(Calendar.MONTH);
+        final short DIA = (short) CALENDARIO.get(Calendar.DAY_OF_MONTH);
+
         Scanner teclado = new Scanner(System.in);
         System.out.println("Cual es tu edad? : ");
         short edad = teclado.nextShort();
@@ -11,21 +22,21 @@ public class Main {
         short diaNacimiento = teclado.nextShort();
         System.out.println("Que mes naciste? (numero de mes): ");
         short mesNacimiento = teclado.nextShort();
-        System.out.println("Introduce que dia es hoy (Numero): ");
-        short dia = teclado.nextShort();
-        System.out.println("Introduce que mes es hoy (Numero): ");
-        short mes = teclado.nextShort();
-        System.out.println("Introduce que año es hoy (Numero): ");
-        short año = teclado.nextShort();
 
-        short añoNacimiento = (short) (año - edad);
+        // Validar la fecha introducida
+        if (diaNacimiento > 31 || diaNacimiento < 1 || mesNacimiento > 12 || mesNacimiento < 1) {
+            System.out.println("Fecha introducida no valida");
+            return;
+        }
 
-        if(mesNacimiento == mes ){
-            if(diaNacimiento < dia){
-                añoNacimiento++;
+        short añoNacimiento = (short) (AÑO - edad);
+
+        if (mesNacimiento > MES) {
+            añoNacimiento--;
+        } else if (mesNacimiento == MES) {
+            if (diaNacimiento > DIA) {
+                añoNacimiento--;
             }
-        } else if (mesNacimiento < mes) {
-            añoNacimiento++;
         }
 
         System.out.println("Naciste el "+ diaNacimiento + "/" + mesNacimiento + "/" + añoNacimiento );
