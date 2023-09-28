ad74c173bd4a9ec606afa3101031cfceac9a46d3
LOSEARDES77 <loseardes77@gmail.com>
2023-09-28 17:39:27 +0200

    Done P01

--- U1_P01_34/src/main/java/org/example/Main.java
+++ U1_P01_34/src/main/java/org/example/Main.java
@@ -9,15 +9,24 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         ArrayList<Integer> juegos = new ArrayList<>();
         ArrayList<Integer> sets = new ArrayList<>();
-        juegos.set(0, 0);
-        juegos.set(1, 0);
-        sets.set(0, 0);
-        sets.set(1, 0);
+        System.out.print("Introduce el numero de sets que se jugaran: ");
+        int numSets = teclado.nextInt();
+        for (int i = 0; i < 2; i++){
+            juegos.add(0);
+            sets.add(0);
+        }
         while (true){
             System.out.print("Quien ha ganado este juego? (A, B): ");
             String ganador = teclado.next();
             juegos = handleJuegos(ganador, juegos);
             sets = handleSets(juegos, sets);
+            if (sets.get(0) == (numSets / 2 + 1)){
+                System.out.println("Ha ganado el jugador A");
+                break;
+            }else if (sets.get(1) == (numSets / 2 + 1)){
+                System.out.println("Ha ganado el jugador B");
+                break;
+            }
         }
     }
 
@@ -26,12 +35,32 @@ public class Main {
             marcador.set(0, (marcador.get(0) + 1));
         }else if (ganador.equalsIgnoreCase("b")){
             marcador.set(1, (marcador.get(1) + 1));
+        }else {
+            System.out.println("Jugador no valido (a/b)");
         }
+        System.out.println("El marcador de juegos es: " + marcador.get(0) + " - " + marcador.get(1));
         return marcador;
     }
 
     private static ArrayList<Integer> handleSets(ArrayList<Integer> juegos, ArrayList<Integer> marcador){
-
+        if (juegos.get(0) == 6 && juegos.get(1) <= 4){
+            marcador.set(0, (marcador.get(0) + 1));
+            juegos.set(0, 0);
+            juegos.set(1, 0);
+        }else if (juegos.get(1) == 6 && juegos.get(0) <= 4){
+            marcador.set(1, (marcador.get(1) + 1));
+            juegos.set(0, 0);
+            juegos.set(1, 0);
+        }else if (juegos.get(0) == 7){
+            marcador.set(0, (marcador.get(0) + 1));
+            juegos.set(0, 0);
+            juegos.set(1, 0);
+        }else if (juegos.get(1) == 7){
+            marcador.set(1, (marcador.get(1) + 1));
+            juegos.set(0, 0);
+            juegos.set(1, 0);
+        }
+        System.out.println("El marcador de sets es: " + marcador.get(0) + " - " + marcador.get(1));
         return marcador;
     }
 }
