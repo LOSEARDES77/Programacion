7a6f88f12e2b6cae3828bd96011d43a4eb2f4ab7
LOSEARDES77 <loseardes77@gmail.com>
2023-10-03 09:47:53 +0200

    Fix 20

--- U1/P02/E_20/src/main/java/org/example/Main.java
+++ U1/P02/E_20/src/main/java/org/example/Main.java
@@ -9,25 +9,26 @@ public class Main {
         do {
             System.out.print("Introduce una contraseña: ");
             password = scanner.nextLine();
-            if (validate(password) == 1)
+            if (validar(password) == 1)
                 System.out.println("Error 1: La contraseña debe tener al menos 2 caracteres");
-            if (validate(password) == 2)
+            if (validar(password) == 2)
                 System.out.println("Error 2: La contraseña debe tener como máximo 10 caracteres");
-            if (validate(password) == 3)
+            if (validar(password) == 3)
                 System.out.println("Error 3: La contraseña no puede contener espacios");
-            if (validate(password) == 4)
+            if (validar(password) == 4)
                 System.out.println("Error 4: La contraseña debe contener al menos un caracter especial (#, @, $, %, &)");
-        } while (validate(password) != 0);
+        } while (validar(password) != 0);
+        System.out.println("Contraseña correcta");
     }
 
-    private static int validate(String password){
+    private static int validar(String password){
         if(password.length() < 2)
             return 1;
         if(password.length() > 10)
             return 2;
         if(password.contains(" "))
             return 3;
-        if(password.contains("#") || password.contains("@") || password.contains("$") || password.contains("%") || password.contains("&"))
+        if(!password.contains("#") && !password.contains("@") && !password.contains("$") && !password.contains("%") && !password.contains("&"))
             return 4;
 
         return 0;
