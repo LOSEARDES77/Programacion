d856ae6b186a74c4eca5f1e1df723fb22c543c05
LOSEARDES77 <loseardes77@gmail.com>
2023-10-23 08:30:59 +0200

    Update Main.java

--- U1/P02/ProgramacionTest/src/main/java/org/example/Main.java
+++ U1/P02/ProgramacionTest/src/main/java/org/example/Main.java
@@ -16,23 +16,4 @@ public class Main {
     public static int randomNumber(){
         return (int) (Math.random() * 6) + 1;
     }
-
-
-
-
-
-
-
-
-
-    /*
-    switch (numero){
-                case 1 -> tiradas[0]++;
-                case 2 -> tiradas[1]++;
-                case 3 -> tiradas[2]++;
-                case 4 -> tiradas[3]++;
-                case 5 -> tiradas[4]++;
-                case 6 -> tiradas[5]++;
-            }
-     */
 }
