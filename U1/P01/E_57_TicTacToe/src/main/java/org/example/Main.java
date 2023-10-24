562bca16e6bcce449fc26924380115e1e6460f98
LOSEARDES77 <loseardes77@gmail.com>
2023-10-24 12:35:15 +0200

    Started TicTacToe

---
+++ U1/P01/E_57_TicTacToe/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+        //while(!grid.isMathOver())
+            System.out.println(grid);
+
+    }
+}
