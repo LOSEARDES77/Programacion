ac7d680e1e1fa32b2fb772045704b69bc374c218
LOSEARDES77 <loseardes77@gmail.com>
2023-11-02 08:30:44 +0100

    Refactoring

--- U1/P02/EntornosTests/src/main/java/org/example/Main.java
+++ U1/P02/EntornosTests/src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Propietario prop1 = new Propietario("12345678F", "Pepe");
+        /*Propietario prop1 = new Propietario("12345678F", "Pepe");
         Animal animal = new Animal(1, "Oso", prop1);
 
         System.out.println(prop1);
-        System.out.println(animal);
+        System.out.println(animal);*/
     }
 }
