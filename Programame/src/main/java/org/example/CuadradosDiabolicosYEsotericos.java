fd88a1628f1e96917ecee399a30340541ec8424c
loseardes77 <loseardes77@gmail.com>
2024-02-13 11:50:57 +0100

    Funciona

--- Programame/src/main/java/org/example/CuadradosDiabolicosYEsotericos.java
+++ Programame/src/main/java/org/example/CuadradosDiabolicosYEsotericos.java
@@ -1,95 +1,163 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Objects;
+import java.util.Scanner;
 
 public class CuadradosDiabolicosYEsotericos {
-    public static void main(String[] args) throws Exception {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        String input;
-        while (!Objects.equals(input = r.readLine(), "0")) {
-            int n = Integer.parseInt(input);
-            int[][] matrix = new int[n][n];
-            String matrixStr = r.readLine();
-            String[] matrixStrArr = matrixStr.split(" ");
-            int k = 0;
+    public static int calcularEsquinas(int[][] m) {
+        int sumaEsquinas = 0;
+        sumaEsquinas += m[0][0];
+        sumaEsquinas += m[0][m.length-1];
+        sumaEsquinas += m[m.length-1][0];
+        sumaEsquinas += m[m.length-1][m.length-1];
+        return sumaEsquinas;
+    }
+
+    public static int calcularCm(int[][] m) {
+        int cm = 0;
+        for (int i = 0; i < m.length; i++) {
+            cm += m[0][i];
+        }
+        return cm;
+    }
+
+    public static int calcularFila(int[][] m, int n) {
+        int sumaFila = 0;
+        for (int i = 0; i < m.length; i++) {
+            sumaFila += m[n][i];
+        }
+        return sumaFila;
+    }
+
+    public static boolean calcularDiagonales(int[][] m, int cm) {
+        int diagonalPrincipal  = 0;
+        int diagonalSecundaria = 0;
+        for (int i = m.length-1, j = 0; i >= 0; i--, j++) {
+            diagonalPrincipal  += m[i][j];
+            diagonalSecundaria += m[j][j];
+        }
+        return (diagonalPrincipal == cm &&
+                diagonalSecundaria == cm);
+    }
+
+    public static int calcularColumna(int[][] m, int n) {
+        int sumaColumna = 0;
+        for (int i = 0; i < m.length; i++) {
+            sumaColumna += m[i][n];
+        }
+        return sumaColumna;
+    }
+
+    public static int calcularCasillasMitad(int[][] m) {
+        int mitad = 0;
+        if (m.length % 2 != 0) {
+            mitad += m[0][m.length/2]; // arriba
+            mitad += m[m.length/2][0]; // izq
+            mitad += m[m.length-1][m.length/2]; // abajo
+            mitad += m[m.length/2][m.length-1]; // der
+        }
+        else {
+            mitad += m[0][m.length/2]; // arriba
+            mitad += m[0][(m.length/2)-1];
+
+            mitad += m[m.length/2][0]; // izq
+            mitad += m[(m.length/2)-1][0];
+
+            mitad += m[m.length-1][m.length/2]; // abajo
+            mitad += m[m.length-1][(m.length/2)-1];
+
+            mitad += m[m.length/2][m.length-1]; // der
+            mitad += m[(m.length/2)-1][m.length-1];
+        }
+        return mitad;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n, cm, cm2;
+        int[][] m;
+        int[] test;
+        boolean diabolico, esoterico;
+
+        while (true) {
+
+            n = s.nextInt();
+            if (n == 0) break;
+
+            m = new int[n][n];
+            test   = new int[n*n];
+
+            diabolico = true;
+            esoterico = true;
+
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    matrix[i][j] = Integer.parseInt(matrixStrArr[k]);
-                    k++;
+                    int numero = s.nextInt();
+                    m[i][j] = numero;
+                    if (numero > (n*n) || test[numero-1] == 1)
+                        esoterico = false;
+                    else if (numero <= (n*n))
+                        test[numero-1] = 1;
                 }
             }
-            System.out.println(isEsoteric(matrix) ? "ESOTERICO" : isDiabolic(matrix) ? "DIABOLICO" : "NO");
-        }
-    }
 
-    private static boolean isEsoteric(int[][] matrix) {
-        if (!isDiabolic(matrix)) {
-            return false;
-        }
-        int cm = 0;
-        for (int[] ints : matrix) {
-            cm += ints[0];
-        }
-        int numCasillas = matrix.length * matrix.length;
-        int cifras = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                cifras += (int) Math.log10(matrix[i][j]) + 1;
+            cm  = calcularCm( m );
+            cm2 = (cm * 4) / n;
+
+            for (int i = 0; i < m.length; i++) {
+                if (calcularFila(m, i) != cm) {
+                    diabolico = false;
+                    break;
+                }
+                if (calcularColumna(m, i) != cm) {
+                    diabolico = false;
+                    break;
+                }
             }
-        }
-        if (cifras != numCasillas) {
-            return false;
-        }
 
-        int cm2 = 4 * cm / matrix.length;
-        int esquinas = matrix[0][0] + matrix[0][matrix.length - 1] + matrix[matrix.length - 1][0] + matrix[matrix.length - 1][matrix.length - 1];
-        if (cm2 != esquinas) {
-            return false;
-        }
+            if (diabolico && !calcularDiagonales(m, cm))
+                diabolico = false;
 
-        if (matrix.length % 2 == 1) {
-            int mitad = matrix.length / 2;
-            int casillas2 = matrix[mitad][0] + matrix[mitad][matrix.length - 1] + matrix[0][mitad] + matrix[matrix.length - 1][mitad];
-            if (cm2 != casillas2) {
-                return false;
+            if (diabolico && esoterico) {
+                if (calcularEsquinas(m) != cm2)
+                    diabolico = false;
+
+                int mitad = calcularCasillasMitad( m );
+                if (n % 2 != 0 && mitad != cm2)
+                    esoterico = false;
+                else if (n % 2 == 0) {
+                    if (mitad != (cm2 * 2))
+                        esoterico = false;
+                }
+
+                if (n % 2 != 0 && esoterico) {
+                    if (m[m.length/2][m.length/2] * 4 != cm2)
+                        esoterico = false;
+                }
+                else if (esoterico) {
+                    int centro = 0;
+                    centro += m[m.length/2][m.length/2];
+                    centro += m[m.length/2][(m.length/2)-1];
+                    centro += m[(m.length/2)-1][m.length/2];
+                    centro += m[(m.length/2)-1][(m.length/2)-1];
+                    if (centro != cm2)
+                        esoterico = false;
+                }
             }
-            int casillaCentral = matrix[mitad][mitad];
-            return cm2 == casillaCentral * 4;
+
+            if (diabolico && esoterico) {
+                System.out.println("ESOTERICO");
+            }
+            else if (diabolico) {
+                System.out.println("DIABOLICO");
+            }
+            else {
+                System.out.println("NO");
+            }
 
         }
-        int[] mitad = new int[]{(matrix.length -1) / 2 - 1, matrix.length / 2};
-        int casillas2 = matrix[mitad[0]][0] + matrix[mitad[0]][matrix.length - 1] + matrix[0][mitad[0]] + matrix[matrix.length - 1][mitad[0]] + matrix[mitad[1]][0] + matrix[mitad[1]][matrix.length - 1] + matrix[0][mitad[1]] + matrix[matrix.length - 1][mitad[1]];
-        if (cm2 * 2 != casillas2) {
-            return false;
-        }
-        int casillaCentral = matrix[mitad[0]][mitad[0]] + matrix[mitad[0]][mitad[1]] + matrix[mitad[1]][mitad[0]] + matrix[mitad[1]][mitad[1]];
-        return cm2 == casillaCentral;
+
     }
-    private static boolean isDiabolic(int[][] matrix) {
-        int cm = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            cm += matrix[i][0];
-        }
-        int cm2 = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            cm2 += matrix[0][i];
-        }
-        if (cm != cm2) {
-            return false;
-        }
-        int cm3 = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            cm3 += matrix[i][i];
-        }
-        if (cm != cm3) {
-            return false;
-        }
-        int cm4 = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            cm4 += matrix[i][matrix.length - 1 - i];
-        }
-        return cm == cm4;
-    }
+
 }
